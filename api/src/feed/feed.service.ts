09d3887033229f8cd00b4febfe3ac6dc0699c526
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-29 10:56:30 +0400

    Add caching to FeedService for improved performance; implement Redis integration to store and retrieve user feed data, reducing database queries on subsequent requests.

--- api/src/feed/feed.service.ts
+++ api/src/feed/feed.service.ts
@@ -2,10 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { and, desc, eq, lt, or } from 'drizzle-orm';
 import { db } from '../db/db';
 import { follows, posts } from '../db/schema';
+import { redis } from 'src/redis/redis';
 
 @Injectable()
 export class FeedService {
   async getFeed(userId: number, limit = 20, cursor?: string) {
+    const cacheKey = `feed:${userId}`;
+
+    const cachedFeed = await redis.get(cacheKey);
+
+    if (cachedFeed) {
+      console.log('CACHE HIT');
+
+      return JSON.parse(cachedFeed) as unknown;
+    }
+
+    console.log('CACHE MISS');
+
     const conditions = [eq(follows.followerId, userId)];
 
     if (cursor) {
@@ -56,10 +69,14 @@ export class FeedService {
           ).toString('base64')
         : null;
 
-    return {
+    const response = {
       data,
       nextCursor,
       hasMore,
     };
+
+    await redis.set(cacheKey, JSON.stringify(response), 'EX', 60);
+
+    return response;
   }
 }
